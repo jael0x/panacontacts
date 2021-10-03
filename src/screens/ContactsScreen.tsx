6518c2ab9d0c034c5fa6b7e54711399fc6c8df84
Jael Armas M <jaelarmas.m@gmail.com>
2021-10-03 13:10:42 -0500

    Load more contacts on scrolll

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
-import { View, ScrollView, RefreshControl } from 'react-native';
+import { View, ScrollView, RefreshControl, NativeScrollEvent } from 'react-native';
 import { makeRequest } from '../utils/axiosClient';
 import RenderContacts, { IContact } from '../components/RenderContacts';
 
@@ -17,19 +17,29 @@ const ContactsScreen: FC = () => {
         loadData();
     }, []);
 
-    const loadData = async () => {
+    const loadData = async (page = 1) => {
         setLoading(true);
-        let endPointUrl = `?page=1&results=20`;
+        let endPointUrl = '?page=' + page + '&results=20';
         makeRequest(endPointUrl)
             .then((contacts) => {
-                setLoading(false);
                 if (contacts) {
                     setPaginationInfo(contacts.info);
-                    setContactList(contacts.results);
+                    if (page === 1)
+                        setContactList(contacts.results);
+                    else
+                        setContactList([...contactList, ...contacts.results]);
                 }
+                setLoading(false);
             });
     }
 
+    const isCloseToBottom = (nativeEvent: NativeScrollEvent) => {
+        const paddingToBottom = 100;
+        const { layoutMeasurement, contentOffset, contentSize } = nativeEvent
+        const position = layoutMeasurement.height + contentOffset.y;
+        const content = contentSize.height - paddingToBottom;
+        return position >= content;
+    };
 
     return (
         <View style={{ flex: 1, width: '100%' }}>
@@ -38,6 +48,13 @@ const ContactsScreen: FC = () => {
                     refreshing={loading}
                     onRefresh={() => loadData()}
                 />}
+                onScroll={({ nativeEvent }) => {
+                    if (isCloseToBottom(nativeEvent)) {
+                        const page = paginationInfo.page + 1;
+                        loadData(page);
+                    }
+                }}
+                scrollEventThrottle={400}
             >
                 <RenderContacts contacts={contactList} />
             </ScrollView>
